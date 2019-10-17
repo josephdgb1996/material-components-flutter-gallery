859d5b94389710dab13e6e82e996789cebad5018
Rami Abou Ghanem <raboughanem@google.com>
2019-10-17 13:46:18 -0400

    Create basic layout for demo page

--- gallery/lib/demo.dart
+++ gallery/lib/demo.dart
@@ -5,10 +5,40 @@ class DemoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.code),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.library_books),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Here is a demo!'),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16.0),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'Demo Placeholder',
+            style: Theme.of(context).textTheme.body1.apply(color: Colors.white),
+          ),
+        ),
       ),
     );
   }
--- gallery/lib/theme_data.dart
+++ gallery/lib/theme_data.dart
@@ -5,23 +5,29 @@ class GalleryThemeData {
   static const _lightFillColor = Colors.black;
   static const _darkFillColor = Colors.white;
 
-  static ThemeData lightThemeData = ThemeData(
-    colorScheme: _lightColorScheme,
-    textTheme: _textTheme,
-    appBarTheme: AppBarTheme(
-      textTheme: _textTheme,
-      color: _lightColorScheme.background,
-    ),
+  static ThemeData lightThemeData = themeData(
+    _lightColorScheme,
+    Brightness.light,
+  );
+  static ThemeData darkThemeData = themeData(
+    _darkColorScheme,
+    Brightness.dark,
   );
 
-  static ThemeData darkThemeData = ThemeData(
-    colorScheme: _darkColorScheme,
-    textTheme: _textTheme,
-    appBarTheme: AppBarTheme(
+  static ThemeData themeData(ColorScheme colorScheme, Brightness brightness) {
+    return ThemeData(
+      colorScheme: colorScheme,
       textTheme: _textTheme,
-      color: _darkColorScheme.background,
-    ),
-  );
+      appBarTheme: AppBarTheme(
+        textTheme: _textTheme,
+        color: colorScheme.background,
+        elevation: 0,
+        iconTheme: IconThemeData(color: colorScheme.primary),
+        brightness: brightness,
+      ),
+      canvasColor: colorScheme.background,
+    );
+  }
 
   static ColorScheme _lightColorScheme = ColorScheme(
     primary: Color(0xFFB93C5D),
